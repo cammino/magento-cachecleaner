a58b48a744bd2bac64339533b9c6ff2e5cb2b17e
Mateus Rasteiro <mateusrasteiro10@gmail.com>
2025-01-21 14:42:09 -0300

    DEV-2559 - Clear fpc cache if product is set to out of stock after order is placed

--- Model/Observer.php
+++ Model/Observer.php
@@ -7,4 +7,36 @@ class Cammino_Cachecleaner_Model_Observer
         Mage::app()->getCacheInstance()->flush();
         Mage::getSingleton('fpc/fpc')->clean();
     }
+
+    public function clearCacheAfterOrder(Varien_Event_Observer $observer) {
+        $order = $observer->getEvent()->getOrder();
+        $clearCacheActive = Mage::getStoreConfig('cachecleaner/cachecleaner_group/cachecleaner_active');
+    
+        if (!empty($clearCacheActive)) {
+            Mage::log('Verificando o estoque dos itens do pedido', null, 'cache_cleaner.log');
+    
+            foreach ($order->getAllItems() as $item) {
+                $productId = $item->getProductId();
+                $product = Mage::getModel('catalog/product')->load($productId);
+    
+                // Skip product if is not a simple product
+                if ($product->getTypeId() !== Mage_Catalog_Model_Product_Type::TYPE_SIMPLE) {
+                    continue;
+                }
+    
+                $stockItem = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
+                if ($stockItem->getQty() <= 0) {
+                    Mage::log(
+                        "O produto '{$product->getName()}' (SKU: {$product->getSku()}) ficou sem estoque após o pedido.",
+                        null,
+                        'cache_cleaner.log'
+                    );
+    
+                    Mage::log('Limpando o cache FPC da loja', null, 'cache_cleaner.log');
+                    Mage::getSingleton('fpc/fpc')->clean();
+                    break;
+                }
+            }
+        }
+    }
 }
